1cfc79e31ef4ad8c31dbf2e3bad14a680143869e
sohyun-ahn <sohyun5747@gmail.com>
2024-08-12 20:25:45 +0900

    Feat: 스케줄 기준 관리 화면 초안

---
+++ apps/control/src/components/management/schedule/FilterContainer.tsx
@@ -0,0 +1,31 @@
+import { SearchOutlined } from '@ant-design/icons';
+import { Dropdown, SearchBar, Button } from '@postcoil/ui';
+import react from 'react';
+
+import styles from './FilterContainer.module.scss';
+
+interface PropsType {
+  process: {
+    title: string; // 공정명
+    options: string[]; // ['1CAL', '2CAL', 'EGL']
+  };
+
+  handleSearch: () => void; // function to handle search button click event
+}
+
+const FilterContainer = () => {
+  const handleSearch = () => {};
+  const handleFilter = () => {};
+
+  return (
+    <div className={styles.filterContainer}>
+      <Dropdown title="공정명" />
+      {/* TODO: Dropdown 데이터 변경 */}
+      <Button text="조회" onClick={handleSearch} />
+      <SearchBar />
+      <Button text="검색" onClick={handleFilter} />
+    </div>
+  );
+};
+
+export default FilterContainer;
