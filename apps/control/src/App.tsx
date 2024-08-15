2f6384b9b947f7768d7a50193239ab2067a4197f
hanriverkitty <danieltls@naver.com>
2024-08-15 18:14:32 +0900

    Refactor: sushi 페이지 적용 네비게이션 메뉴 외부에서  불러올 수 있게 한뒤, sushi 페이지 적용해봤습니다 페이지적용하니까 스크롤바 또 생기는데, 나중에 css 다시 적용해야 할듯

--- apps/control/src/App.tsx
+++ apps/control/src/App.tsx
@@ -1,18 +1,21 @@
-import { Navigation } from '@postcoil/ui';
+import { Navigation, Items, ContentBoard } from '@postcoil/ui';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import human from './assets/human.svg';
 import logo from './assets/logo.svg';
 import './styles/global.scss';
-
+import ExtMPage from './pages/management/extraction/ExtMPage';
+import SchMPage from './pages/management/schedule/SchMPage';
+// package/common-ui/config 안의 일을 만져서 네이게이션에 들어갈 부분 수정가능
+// <ContentBoard Board={ExampleJoUp} /> 이부분에서 ExampleJoUp 에 당신의 페이지를 넣으면 됩니다
 function App() {
   return (
     <Router>
       <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
-        <Navigation logo={logo} human={human} />
+        <Navigation logo={logo} human={human} menuItems={Items} />
         <Routes>
-          <Route path="/control1" />
-          <Route path="/control2" />
+          <Route path="/control1" element={<ContentBoard Board={ExtMPage} />} />
+          <Route path="/control2" element={<ContentBoard Board={SchMPage} />} />
         </Routes>
       </div>
     </Router>
