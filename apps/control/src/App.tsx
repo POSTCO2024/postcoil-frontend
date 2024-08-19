4bd468dc795a074a6cf54c5d9f34d0f4d8721b35
hanriverkitty <98452384+hanriverkitty@users.noreply.github.com>
2024-08-19 22:42:14 +0900

    Merge: feat/fc-003/pinky (#30)
    * Refactor: 루트페이지가 '/'라 첫화면이 안보이던 이슈를 해결
    이제 앱을 실행하면 컨트롤의 첫화면은 무조건 'control1' 입니다
    
    * Feat: 공장별 모니터링 화면의 변환 그래프 제작
    토글버튼을 통해 그래프를 애니메이션으로 바뀌게할 수 있다
    
    Fixes: 변환 시 이전 정보가 그대로 남아있는 이슈 수정중
    
    * Fix: 이전 정보가 그대로 남아있는 이슈 수정
    이제 그래프가 원활하게 잘 바뀝니다. 또한 hover시 강조 효과가 있습니다
    
    * Remove: Chart.tsx 파일 삭제
    
    * Fix: echarts dependency 추가 및 StockCharts 파일 수정
    에러로 인해 PR 안되던 문제 해결
    
    * Chore: pnpm-lock.yaml 추가 커밋

--- apps/control/src/App.tsx
+++ apps/control/src/App.tsx
@@ -1,22 +1,29 @@
-import { Navigation, ContentBoard } from '@postcoil/ui';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ContentBoard, Navigation } from '@postcoil/ui';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 
 import human from './assets/human.svg';
 import logo from './assets/logo.svg';
 import { MenuItems } from './config/MenuItems';
 import './styles/global.scss';
-import { Board } from './pages/fc001a/Board/Borad';
-import { TopBar } from './pages/fc001a/TopBar/TopBar';
-// package/common-ui/config 안의 일을 만져서 네이게이션에 들어갈 부분 수정가능
-// <ContentBoard Board={ExampleJoUp} /> 이부분에서 ExampleJoUp 에 당신의 페이지를 넣으면 됩니다
+import StockCharts from './pages/monitoring/stock/StockCharts';
+
 function App() {
   return (
     <Router>
       <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
         <Navigation logo={logo} human={human} menuItems={MenuItems} />
         <Routes>
-          <Route path="/control1" element={<ContentBoard Board={Board} />} />
-          <Route path="/control2" element={<ContentBoard Board={TopBar} />} />
+          <Route path="/" element={<Navigate replace to="/control1" />} />
+          <Route
+            path="/control1"
+            element={<ContentBoard Board={StockCharts} />}
+          />
+          <Route path="/control2" />
         </Routes>
       </div>
     </Router>
---
+++ apps/control/src/pages/monitoring/stock/StockCharts.tsx
@@ -0,0 +1,117 @@
+import * as echarts from 'echarts';
+import React, { useEffect, useRef, useState } from 'react';
+
+const treemapData = [
+  { name: 'Max Flow Min Cut', value: 7840 },
+  { name: 'Link Distance', value: 5731 },
+  { name: 'Shortest Paths', value: 5914 },
+  { name: 'Betweenness Centrality', value: 3534 },
+  { name: 'Spanning Tree', value: 3416 },
+  { name: 'Hierarchical Cluster', value: 6714 },
+  { name: 'Aspect Ratio Banker', value: 7074 },
+  { name: 'Agglomeration Cluster', value: 3938 },
+  { name: 'Community Structure', value: 3812 },
+];
+
+treemapData.sort(function (a, b) {
+  return a.value - b.value;
+});
+const treemapOption: echarts.EChartsOption = {
+  series: [
+    {
+      type: 'treemap',
+      data: treemapData.map((item, index) => ({
+        name: item.name,
+        value: item.value,
+        itemStyle: {
+          // 색상 지정 후 하나씩 가져오기
+          color: [
+            '#FFADAD',
+            '#FFD6A5',
+            '#FDFFB6',
+            '#CAFFBF',
+            '#9BF6FF',
+            '#A0C4FF',
+            '#BDB2FF',
+            '#FFC6FF',
+            '#FFFFFC',
+          ][8 - (index % 9)],
+        },
+      })),
+      label: {
+        color: 'black',
+        fontWeight: 'bolder',
+      },
+      emphasis: {
+        itemStyle: {
+          // hover시 배경화면
+          color: 'white',
+        },
+        label: {
+          show: true,
+          // 해당 표에 손을 놓으면 개수 표시
+          formatter: function (graph) {
+            return `${graph.name} : ${graph.value} 개`;
+          },
+        },
+      },
+      universalTransition: true,
+    },
+  ],
+};
+const barOption: echarts.EChartsOption = {
+  xAxis: {
+    type: 'value',
+  },
+  yAxis: {
+    type: 'category',
+    data: treemapData.map((item) => item.name),
+  },
+  series: [
+    {
+      type: 'bar',
+      data: treemapData.map((item) => item.value),
+      universalTransition: true,
+    },
+  ],
+};
+
+const StockCharts: React.FC = () => {
+  const chartInstanceRef = useRef<echarts.EChartsType | null>(null);
+  const [isTreemap, setIsTreemap] = useState(true);
+  const chartRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (chartRef.current) {
+      const instance = echarts.init(chartRef.current);
+      chartInstanceRef.current = instance;
+      instance.setOption(treemapOption);
+
+      return () => {
+        instance.dispose();
+      };
+    }
+    return undefined;
+  }, []);
+
+  useEffect(() => {
+    if (chartInstanceRef.current) {
+      chartInstanceRef.current.setOption(
+        isTreemap ? treemapOption : barOption,
+        true,
+      );
+    }
+  }, [isTreemap]);
+
+  const toggleChart = () => {
+    setIsTreemap((prev) => !prev);
+  };
+
+  return (
+    <div style={{ height: '100%' }}>
+      <button onClick={toggleChart}>그래프 변환</button>
+      <div ref={chartRef} style={{ width: '100%', height: '50%' }} />
+    </div>
+  );
+};
+
+export default StockCharts;
