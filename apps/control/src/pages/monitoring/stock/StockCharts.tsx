213b5d74ca03cbbe8a780e64b1bbdd116acbf7fe
hanriverkitty <danieltls@naver.com>
2024-08-20 02:02:32 +0900

    Refactor: 컴포넌트 분리!, treemap 그래프 기능 일부 수정 treemap에서 클릭시 화면 안움직이고, 그래프 줌인 줌아웃 기능 삭제

---
+++ apps/control/src/pages/monitoring/stock/StockCharts.tsx
@@ -0,0 +1,44 @@
+import * as echarts from 'echarts';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { barOption, treemapOption } from '@/config/chartOption/ChartOption';
+
+const StockCharts: React.FC = () => {
+  const chartInstanceRef = useRef<echarts.EChartsType | null>(null);
+  const [isTreemap, setIsTreemap] = useState(true);
+  const chartRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (chartRef.current) {
+      const instance = echarts.init(chartRef.current);
+      chartInstanceRef.current = instance;
+      instance.setOption(treemapOption);
+
+      return () => {
+        instance.dispose();
+      };
+    }
+    return undefined;
+  }, []);
+
+  useEffect(() => {
+    if (chartInstanceRef.current) {
+      chartInstanceRef.current.setOption(
+        isTreemap ? treemapOption : barOption,
+        true,
+      );
+    }
+  }, [isTreemap]);
+
+  const toggleChart = () => {
+    setIsTreemap((prev) => !prev);
+  };
+
+  return (
+    <div style={{ height: '100%' }}>
+      <button onClick={toggleChart}>그래프 변환</button>
+      <div ref={chartRef} style={{ width: '100%', height: '50%' }} />
+    </div>
+  );
+};
+
+export default StockCharts;
