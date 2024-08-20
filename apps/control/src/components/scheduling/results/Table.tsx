aa7c3fd21157456dd8f6e32056bcebe91ac51944
hanriverkitty <danieltls@naver.com>
2024-08-21 01:22:25 +0900

    Feat: schedule 이력 탭에서 스케줄을 누르면 해당하는 스케줄의 롤단위별 코일 리스트를 확인할 수 있다
    Fixes: 스케줄을 누르면 누른 스케줄의 롤 조회 화면이 뜨는데, 이때 스케줄 아이디를
    롤 조회화면으로 전달할 방법을 모색해야된다

---
+++ apps/control/src/components/scheduling/results/Table.tsx
@@ -0,0 +1,72 @@
+import { Table as AntTable } from 'antd';
+import { TableProps } from 'antd/es/table'; // Table
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import styles from './Table.module.scss';
+import { createColumns } from './TableUtils';
+
+// info
+// API 결과를 columns, data에 저장하여 사용
+// props1: CheckBox 사용 여부를 props를 이용하여 useCheckBox에 true/false로 전달해준다.
+// props2: 테이블의 내용(columns, data)을 json형식으로 전달해준다.
+// ex) <Table useCheckBox={true} columns={columnsData} data={tableData} />
+
+// Dataset Type
+interface DataType {
+  key: string;
+  no: string;
+  [key: string]: unknown;
+  schduleId: string;
+  createdDate: string;
+  rollID: string;
+  facility: string;
+  startTime: string;
+  endTime: string;
+}
+
+interface TableComponentProps {
+  useCheckBox: boolean;
+  // Config
+  columns: Partial<DataType>[];
+  data: DataType[];
+}
+
+// Column Event
+const onTableChange: TableProps<DataType>['onChange'] = (
+  pagination,
+  filters,
+  sorter,
+  extra,
+) => {
+  console.log('params', pagination, filters, sorter, extra);
+};
+
+export const Table: React.FC<TableComponentProps> = ({
+  useCheckBox,
+  columns,
+  data,
+}) => {
+  const processedColumns = createColumns(useCheckBox, columns);
+  const navigate = useNavigate();
+  return (
+    <div className={styles.tableContainer}>
+      <AntTable
+        className={styles.antTable}
+        columns={processedColumns}
+        dataSource={data}
+        onChange={onTableChange}
+        onRow={(record) => {
+          return {
+            onClick: () => {
+              console.log(record);
+              navigate(`/roll/${record.schduleId}`);
+            },
+          };
+        }}
+      />
+    </div>
+  );
+};
+
+export default Table;
--- apps/control/src/pages/fs004/Fs004a.tsx
+++ apps/control/src/pages/fs004/Fs004a.tsx
@@ -1,4 +1,3 @@
-import { Table } from '@postcoil/ui';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
