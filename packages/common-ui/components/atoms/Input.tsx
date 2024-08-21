de8b0a6e2ae0a06e49839cb200480cf2739c4aa9
dudco0040 <dudco0040@gmail.com>
2024-08-21 21:29:52 +0900

    Rename: 파일명 오타 수정

--- packages/common-ui/components/atoms/Input.tsx
+++ packages/common-ui/components/atoms/Input.tsx
@@ -1,12 +1,12 @@
 import { Input as AntInput, Space } from 'antd';
 import React from 'react';
 
-// import styles from './Input.modul.scss';
+import styles from './Input.module.scss';
 
 export const Input: React.FC = () => {
   return (
-    <Space.Compact>
-      <AntInput />
+    <Space.Compact className={styles.inputContainer}>
+      <AntInput className={styles.input} />
     </Space.Compact>
   );
 };
