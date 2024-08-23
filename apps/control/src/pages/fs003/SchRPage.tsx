f2d1bc48aaaaaa84645f711cdd72745a71a575e1
Sohyun Ahn <71028309+sohyun-ahn@users.noreply.github.com>
2024-08-23 04:33:45 -0700

    Merge: Feat/fs003/ash (#68)
    Feat: 스케줄 결과 화면 v2 (#68)

--- apps/control/src/pages/fs003/SchRPage.tsx
+++ apps/control/src/pages/fs003/SchRPage.tsx
@@ -5,6 +5,7 @@ import styles from './SchRPage.module.scss';
 
 import ContentContainer from '@/pages/fs003/result/ContentContainer';
 import DraggableChart from '@/pages/fs003/result/DraggableChart';
+import DraggableChart2 from '@/pages/fs003/result/DraggableChart2';
 import FilterContainer from '@/pages/fs003/result/FilterContainer';
 
 const SchRPage = () => {
@@ -22,7 +23,31 @@ const SchRPage = () => {
         <Tab labels={['그래프', '리스트']} onChange={handleTabChange} />
       </section>
       <div className={styles.result}>
-        {isGraphVisible ? <DraggableChart /> : <ContentContainer />}
+        {isGraphVisible ? (
+          <div className={styles.charts}>
+            <DraggableChart2 />
+            <DraggableChart />
+          </div>
+        ) : (
+          <ContentContainer />
+        )}
+        <div
+          className={styles.summary}
+          style={{
+            width: '100%',
+            height: '30%',
+            backgroundColor: 'lightgray',
+            position: 'absolute',
+            bottom: '10px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: '2.5rem',
+            textAlign: 'center',
+            borderRadius: '10px',
+          }}>
+          스케줄 재료 분석
+        </div>
       </div>
     </div>
   );
--- apps/control/src/pages/fs003/result/DraggableChart.tsx
+++ apps/control/src/pages/fs003/result/DraggableChart.tsx
@@ -13,17 +13,44 @@ variwide(Highcharts);
 interface DataPoint {
   name: string;
   y: number; // length에 해당하는 값
-  z: number; // width에 해당하는 값
+  z: number; // Thickness에 해당하는 값
   x: number; // sequence number
   changed?: boolean; // is the point changed by user?
 }
 
 const initialData: DataPoint[] = [
-  { name: '1', y: 50.2, z: 20, x: 1 },
-  { name: '2', y: 42, z: 30, x: 2, changed: true },
-  { name: '3', y: 39.2, z: 40, x: 3 },
-  { name: '4', y: 38, z: 35, x: 4 },
-  { name: '5', y: 35.6, z: 25, x: 5 },
+  { name: 'C1CAL0001A', y: 7.3, z: 30, x: 1 },
+  { name: 'C1CAL0002B', y: 6.2, z: 45, x: 2, changed: true },
+  { name: 'C1CAL0003A', y: 5.8, z: 50, x: 3 },
+  { name: 'C1CAL0004B', y: 7.1, z: 25, x: 4 },
+  { name: 'C1CAL0005A', y: 6.4, z: 35, x: 5 },
+  { name: 'C1CAL0006B', y: 5.9, z: 40, x: 6 },
+  { name: 'C1CAL0007A', y: 7.2, z: 20, x: 7 },
+  { name: 'C1CAL0008B', y: 6.8, z: 15, x: 8 },
+  { name: 'C1CAL0009A', y: 5.7, z: 55, x: 9 },
+  { name: 'C1CAL0010B', y: 6.5, z: 50, x: 10 },
+
+  { name: 'C1CAL0011A', y: 7.4, z: 60, x: 11 },
+  { name: 'C1CAL0012B', y: 5.6, z: 22, x: 12 },
+  { name: 'C1CAL0013A', y: 6.1, z: 48, x: 13 },
+  { name: 'C1CAL0014B', y: 7.0, z: 36, x: 14 },
+  { name: 'C1CAL0015A', y: 5.8, z: 34, x: 15 },
+  { name: 'C1CAL0016B', y: 6.7, z: 43, x: 16 },
+  { name: 'C1CAL0017A', y: 7.3, z: 29, x: 17 },
+  { name: 'C1CAL0018B', y: 5.5, z: 18, x: 18 },
+  { name: 'C1CAL0019A', y: 6.9, z: 50, x: 19 },
+  { name: 'C1CAL0020B', y: 5.8, z: 55, x: 20 },
+
+  { name: 'C1CAL0021A', y: 6.4, z: 39, x: 21 },
+  { name: 'C1CAL0022B', y: 7.1, z: 49, x: 22 },
+  { name: 'C1CAL0023A', y: 5.7, z: 28, x: 23 },
+  { name: 'C1CAL0024B', y: 6.6, z: 60, x: 24 },
+  { name: 'C1CAL0025A', y: 7.2, z: 45, x: 25 },
+  { name: 'C1CAL0026B', y: 5.9, z: 53, x: 26 },
+  { name: 'C1CAL0027A', y: 6.3, z: 20, x: 27 },
+  { name: 'C1CAL0028B', y: 7.0, z: 25, x: 28 },
+  { name: 'C1CAL0029A', y: 6.7, z: 40, x: 29 },
+  { name: 'C1CAL0030B', y: 5.8, z: 10, x: 30 },
 ];
 
 interface PropsType {
@@ -41,6 +68,10 @@ const DraggableChart: React.FC = ({ filteredData }: PropsType) => {
     const chartOptions: Highcharts.Options = {
       chart: {
         type: 'variwide',
+        margin: [0, 0, 0, 30], // 마진 설정
+        spacing: [0, 0, 0, 0], // 패딩 설정
+        borderWidth: 0,
+        // backgroundColor: 'lightgray',
       },
       title: {
         text: 'Draggable Variwide Chart',
@@ -48,11 +79,16 @@ const DraggableChart: React.FC = ({ filteredData }: PropsType) => {
       },
       xAxis: {
         type: 'category',
+        labels: {
+          enabled: false, // X축 라벨 숨기기
+        },
       },
       yAxis: {
         title: {
-          text: 'Width',
+          text: 'Thickness',
         },
+        reversed: true, // Y축 반전
+        max: 15, // Y축 최대값 설정
       },
 
       legend: {
@@ -76,11 +112,26 @@ const DraggableChart: React.FC = ({ filteredData }: PropsType) => {
           },
           tooltip: {
             pointFormat:
-              'Width: <b> {point.y} mm</b><br>' +
+              'Thickness: <b> {point.y} mm</b><br>' +
               'W_Time: <b> {point.z} mins</b><br>',
           },
         },
       },
+      // responsive: {
+      //   rules: [
+      //     {
+      //       condition: {
+      //         maxWidth: 800,
+      //       },
+      //       chartOptions: {
+      //         chart: {
+      //           width: '100%', // 특정 ��면 크기에서 반��형 ��비 조정
+      //           height: '50%', // 특정 화면 크기에서 반응형 높이 조정
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
     };
 
     Highcharts.chart('container', chartOptions);
@@ -88,7 +139,9 @@ const DraggableChart: React.FC = ({ filteredData }: PropsType) => {
 
   return (
     <div className={styles.graph}>
-      <div id="container"></div>
+      <div
+        id="container"
+        style={{ width: '100%', height: '100%', marginTop: '-20px' }}></div>
     </div>
   );
 };
