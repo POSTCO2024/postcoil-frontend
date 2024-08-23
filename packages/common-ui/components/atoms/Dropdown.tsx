6f2071fbcf0dfc4f7f4b8b81925b3de2abb19699
Young Chae Lee <65876884+dudco0040@users.noreply.github.com>
2024-08-23 20:19:26 +0900

    Merge: Feat/fc002av2/leeyc (#64)
    * Feat: 세부 검색 기능 추가
    
    * Feat: 공통 컴포넌트 추가
    
    * Refactor: 공통 컴포넌트 수정
    
    * Rename: 파일명 오타 수정
    
    * Chore: 코드 정리
    
    * Test: 재료 DB 임의 데이터 적용 테스트
    
    * Test: 데이터 리셋
    
    * Chore: PR Lint 에러 해결
    
    * Chore: PR Type 에러 해결

--- packages/common-ui/components/atoms/Dropdown.tsx
+++ packages/common-ui/components/atoms/Dropdown.tsx
@@ -53,7 +53,9 @@ export const Dropdown: React.FC<DropdownProps> = ({
         options={options}
         onChange={
           onChange
-            ? onChange
+            ? (value) => {
+                onChange(value); // `undefined`를 허용
+              }
             : () => {
                 console.log('Dropdown Click');
               }
