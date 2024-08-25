51217d42a9040253ec1fc01617085013c46b763b
sohyun-ahn <sohyun5747@gmail.com>
2024-08-25 22:50:22 +0900

    Feat: 클래스이름 기능 추가

--- packages/common-ui/config/TableConfig.ts
+++ packages/common-ui/config/TableConfig.ts
@@ -2,19 +2,21 @@ import { ColumnType } from 'antd/es/table';
 
 // Dataset Type
 export interface DataType {
-  key: string;
-  no?: string;
+  key?: string;
+  no?: string | number;
   id?: string;
   [key: string]: unknown;
 }
 
 // ColumnDataType
 export interface ColumnDataType<T> extends ColumnType<T> {
+  no?: string | number;
   title: string | JSX.Element;
   dataIndex: string;
+  key?: string;
   sortable?: // sorting 여부
   | boolean // true일 경우 그냥 기본 sorting 함수 사용됨 (grouping sorting 불가)
-    | { compare: (a: T, b: T) => number; multiple?: number }; // sorting custom 함수, grouping sorting 가능
+    | { compare: (a: any, b: any) => number; multiple?: number }; // sorting custom 함수, grouping sorting 가능
   fixed?: boolean | 'left' | 'right'; // fix 여부, true일 경우 'left'로, string일 경우 ('right') 로 고정
   width?: number | string;
   otherProps?: { [key: string]: any }; // 기타 props
--- packages/common-ui/utils/TableUtils.tsx
+++ packages/common-ui/utils/TableUtils.tsx
@@ -13,10 +13,10 @@ export const createColumns = <T extends DataType>(
       key: column.dataIndex as string,
       sorter: column.sortable
         ? typeof column.sortable === 'boolean'
-          ? (a: T, b: T) =>
+          ? (a: any, b: any) =>
               a[column.dataIndex]! > b[column.dataIndex]! ? 1 : -1
           : {
-              compare: column.sortable.compare as (a: T, b: T) => number,
+              compare: column.sortable.compare as (a: any, b: any) => number,
               multiple: column.sortable.multiple || 1,
             }
         : undefined,
