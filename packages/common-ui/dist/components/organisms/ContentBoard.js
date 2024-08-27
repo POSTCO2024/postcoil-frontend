16a907adc192f3bd42cd70365a8de02de5f965c2
hanriverkitty <danieltls@naver.com>
2024-08-27 20:17:00 +0900

    Chore: 빌드 스크립트 수정 및 빌드...

--- packages/common-ui/dist/components/organisms/ContentBoard.js
+++ packages/common-ui/dist/components/organisms/ContentBoard.js
@@ -1,5 +1,5 @@
 import { jsx as _jsx } from "react/jsx-runtime";
-import styles from './ContentBoard.module.css';
+import styles from './ContentBoard.module.scss';
 export function ContentBoard({ Board }) {
     return (_jsx("div", { className: styles.greyBack, children: _jsx("div", { className: styles.whiteBoard, children: Board && _jsx(Board, {}) }) }));
 }
