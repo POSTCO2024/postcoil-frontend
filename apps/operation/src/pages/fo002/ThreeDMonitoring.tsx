3c9b28ede4be751d35584b993afc5cafbbfe5254
sohyun-ahn <sohyun5747@gmail.com>
2024-08-28 09:06:05 +0900

    Rename: folder명 수정

---
+++ apps/operation/src/pages/fo002/ThreeDMonitoring.tsx
@@ -0,0 +1,11 @@
+import styles from './ThreeDMonitoring.module.scss';
+
+const ThreeDMonitoring = () => {
+  return (
+    <div className={styles.page}>
+      <h1>3D 모니터링 작업 화면</h1>
+    </div>
+  );
+};
+
+export default ThreeDMonitoring;
