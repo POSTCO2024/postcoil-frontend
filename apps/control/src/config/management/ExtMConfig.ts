4dd0612f1dad7dd0b9736921f859f2a31a660292
hanriverkitty <danieltls@naver.com>
2024-09-09 02:48:31 +0900

    Feat: 추출기준 조회 및 수정 API 매핑 완료 추출기준 조회하고 수정해서 실시간 반영되는 기능 추가했습니다

--- apps/control/src/config/management/ExtMConfig.ts
+++ apps/control/src/config/management/ExtMConfig.ts
@@ -15,12 +15,27 @@ export const columnsData = [
     dataIndex: 'value',
   },
 ];
-export const standardData = [
-  { key: '1', id: '1', columnName: '공장코드', value: 'L' },
-  { key: '2', id: '2', columnName: '공정코드', value: '1CAL' },
-  { key: '3', id: '3', columnName: '재료상태구분', value: '2' },
-  { key: '4', id: '4', columnName: '진도구분', value: 'D' },
-];
+
+// export const standardData = [
+//   { key: '1', id: '1', columnName: '공장코드', value: 'L' },
+//   { key: '2', id: '2', columnName: '공정코드', value: '1CAL' },
+//   { key: '3', id: '3', columnName: '재료상태구분', value: '2' },
+//   { key: '4', id: '4', columnName: '진도구분', value: 'D' },
+// ];
+
+export const columnMapping: { [key: string]: string } = {
+  factory_code: '공장코드',
+  curr_proc: '공정코드',
+  material_status: '재료상태구분',
+  progress: '진도구분',
+};
+
+export const keyMapping: { [key: string]: string } = {
+  공장코드: 'factoryCode',
+  재료상태구분: 'materialStatus',
+  진도구분: 'progress',
+  공정코드: 'currProc',
+};
 
 export const facilityData = [
   { key: '1', id: '1', facilityId: '1PCM' },
--- apps/control/src/pages/fmc001/extraction/Result.tsx
+++ apps/control/src/pages/fmc001/extraction/Result.tsx
@@ -1,16 +1,56 @@
 import { Button } from '@postcoil/ui';
+import { url } from '@postcoil/ui/config/UrlConfig';
 import { Input, Form } from 'antd';
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 import styles from './Result.module.scss';
 
 import CommonModal from '@/components/common/CommonModal';
+import { keyMapping } from '@/config/management/ExtMConfig';
+
 interface PropsType {
   title: string;
   data: any;
+  facility: string;
+  setPostedData: any;
 }
 
-const Result = ({ title, data }: PropsType) => {
+function transformData(data: any) {
+  const transformedData: { [key: string]: any } = {};
+  // 원본 객체의 각 키를 매핑된 새로운 키로 변환
+  Object.keys(data).forEach((key) => {
+    const newKey = keyMapping[key]; // 매핑된 새로운 키
+    if (newKey) {
+      transformedData[newKey] = data[key]; // 새로운 키에 값을 할당
+    }
+  });
+  return transformedData;
+}
+
+// modal을 수정하고 부모의 setStandardDatas 에 맞는 형태로 바꿔주는 함수
+function updateValues(dataArray: any, newValues: any) {
+  return dataArray.map((item: any) => {
+    // 새로운 값을 찾기
+    const newValue = newValues[item.columnName];
+    return {
+      ...item, // 기존 속성을 그대로 복사
+      value: newValue !== undefined ? newValue : item.value, // 새로운 값이 있으면 업데이트, 없으면 기존 값 유지
+    };
+  });
+}
+
+const Result = ({ title, data, facility, setPostedData }: PropsType) => {
+  const postStandard = async (data: any) => {
+    await axios
+      .post(url + '/control/management/extraction/' + facility, data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   const [form] = Form.useForm();
   const [modalTitle, setModalTitle] = useState('');
   // const [isEditing, setIsEditing] = useState(false); // FIXME: check Remove this
@@ -26,18 +66,38 @@ const Result = ({ title, data }: PropsType) => {
   };
 
   const handleApply = () => {
-    // TODO: save data to server
-    // form.submit();
-    console.log('Success:');
-    setIsModalOpen(false);
+    form
+      .validateFields()
+      .then((values) => {
+        onFinish(values); // Call onFinish with the form values
+        setIsModalOpen(false); // Close the modal after applying changes
+      })
+      .catch((info) => {
+        console.log('Validate Failed:', info);
+      });
   };
 
   // TODO: 조회 common 컴포넌트꺼 사용! - 롤편성조회 화면 지우기
-
+  // 모달창의 수정 버튼을 눌렀을 때 이벤트
   const onFinish = (values: any) => {
+    // 수정된 값
     console.log('Success:', values);
+    // 백엔드 데이터베이스에 맞춰 매핑
+    console.log(transformData(values));
+    postStandard(transformData(values));
+    setPostedData(updateValues(data, values));
     setIsModalOpen(false);
   };
+
+  // 각 버튼 누를때마다 모달 안의 내용이 바뀌게 만듬
+  useEffect(() => {
+    form.setFieldsValue(
+      data.reduce((acc: any, item: any) => {
+        acc[item.columnName] = item.value;
+        return acc;
+      }, {}),
+    );
+  }, [data, form]);
 
   return (
     <div className={styles.resultBtns}>
