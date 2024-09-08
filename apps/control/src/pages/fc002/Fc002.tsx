84629b811c0e4342235e9b8c1ff72923034e0bd8
dudco0040 <dudco0040@gmail.com>
2024-09-08 23:31:57 +0900

    Feat: 에러재 조회 화면 1차 연결

--- apps/control/src/pages/fc002/Fc002.tsx
+++ apps/control/src/pages/fc002/Fc002.tsx
@@ -1,15 +1,33 @@
 import { Table } from '@postcoil/ui';
 import { Button } from 'antd';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 import styles from './Fc002.module.scss';
 import { TopBar } from './topBar/TopBar';
 
 import CommonModal from '@/components/common/CommonModal';
 import { columnsData, tableData } from '@/config/control/Fc002Utils';
+import { aw } from 'vitest/dist/chunks/reporters.C_zwCd4j';
+
+async function getErrorMaterialData() {
+  const url = `http://localhost:8086/control/error`;
+  try {
+    const response = await axios.get(url);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
 
 export const Fc002: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [isValue, setIsValue] = useState(true); // 기본값을 true로 설정(첫페이지)
+  const [dataList, setDataList] = useState<any[]>([]); // API로부터 받을 데이터 상태
+
   const handleModalOpen = () => setIsModalOpen(true);
   const handleOk = () => {
     setIsModalOpen(false);
@@ -18,6 +36,15 @@ export const Fc002: React.FC = () => {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
+
+  // 에러재 목록 조회
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getErrorMaterialData();
+      setDataList(data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className={styles.boardContainer}>
@@ -28,7 +55,7 @@ export const Fc002: React.FC = () => {
           <Table
             useCheckBox={true}
             columns={columnsData}
-            data={tableData}
+            data={dataList.map((item) => ({ ...item, key: item.id }))}
             scroll={{ x: 'max-content', y: 600 }}
             tableLayout={'fixed'}
           />
