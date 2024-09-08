510e41d15e8db5abca0f007bfc0e9cf4570691cc
dudco0040 <dudco0040@gmail.com>
2024-09-08 21:28:45 +0900

    Fix: 드롭박스 값 입력 시, 작업대상재가 추출되는 문제 해결

--- apps/control/src/pages/fc001/Fc001.tsx
+++ apps/control/src/pages/fc001/Fc001.tsx
@@ -43,6 +43,7 @@ export const Fc001: React.FC = () => {
   const label = ['리스트', '표']; // Tab
   const [isValue, setIsValue] = useState(true); // 기본값을 true로 설정(첫페이지)
   const [dataList, setDataList] = useState<any[]>([]); // API로부터 받을 데이터 상태
+  const [targetMaterialData, setTargetMaterialData] = useState<any[]>([]);
   const [selectedProcessCode, setSelectedProcessCode] =
     useState<string>('1PCM'); // 선택된 공정
 
@@ -53,15 +54,22 @@ export const Fc001: React.FC = () => {
   // 컴포넌트가 마운트될 때 API로부터 데이터를 가져옴
   useEffect(() => {
     const fetchData = async () => {
-      // 1. 작업대상재 추출
-      await fetchTargetMaerialData();
+      // 1. 작업대상재 추출 및 저장
+      const target = await fetchTargetMaerialData();
+      setTargetMaterialData(target);
+    };
 
-      // 2. 작업대상재 목록 조회
+    fetchData(); // 함수 호출
+  }, []);
+
+  useEffect(() => {
+    const fetchListData = async () => {
+      // 2. 1번 완료 후, 작업대상재 목록 조회
       const data = await getTable(selectedProcessCode);
       setDataList(data); // 가져온 데이터를 상태에 저장
     };
 
-    fetchData(); // 함수 호출
+    fetchListData();
   }, [selectedProcessCode]);
 
   const handleDropdownChange = (processCode: string) => {
