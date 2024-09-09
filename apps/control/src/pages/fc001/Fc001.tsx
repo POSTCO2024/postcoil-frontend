41b1c95d318ff3635e0247a90fcde9bb46804835
Young Chae Lee <65876884+dudco0040@users.noreply.github.com>
2024-09-09 10:05:40 +0900

    Merge: Feat/fc001a-table-api/leeyc (#105)
    * Feat: 작업대상재 추출 API요청(ing)
    
    * Fix: 드롭박스 값 입력 시, 작업대상재가 추출되는 문제 해결
    
    * Fix: lint 에러 해결

--- apps/control/src/pages/fc001/Fc001.tsx
+++ apps/control/src/pages/fc001/Fc001.tsx
@@ -13,6 +13,20 @@ import {
   dataTable,
 } from '@/config/control/Fc001Utils';
 
+// 작업대상재 추출 및 저장
+async function fetchTargetMaerialData() {
+  const url = `http://localhost:8086/control/target`;
+  try {
+    const response = await axios.get(url);
+    console.log('작업 대상재 등록: ' + response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+// 작업대상재 목록(List) 조회
 async function getTable(processCode: string) {
   const url = `http://localhost:8086/control/fc001a/list/${processCode}`;
   try {
@@ -29,6 +43,7 @@ export const Fc001: React.FC = () => {
   const label = ['리스트', '표']; // Tab
   const [isValue, setIsValue] = useState(true); // 기본값을 true로 설정(첫페이지)
   const [dataList, setDataList] = useState<any[]>([]); // API로부터 받을 데이터 상태
+  // const [targetMaterialData, setTargetMaterialData] = useState<any[]>([]);
   const [selectedProcessCode, setSelectedProcessCode] =
     useState<string>('1PCM'); // 선택된 공정
 
@@ -39,14 +54,25 @@ export const Fc001: React.FC = () => {
   // 컴포넌트가 마운트될 때 API로부터 데이터를 가져옴
   useEffect(() => {
     const fetchData = async () => {
+      // 1. 작업대상재 추출
+      await fetchTargetMaerialData();
+    };
+
+    fetchData(); // 함수 호출
+  }, []);
+
+  useEffect(() => {
+    const fetchListData = async () => {
+      // 2. 1번 완료 후, 작업대상재 목록 조회
       const data = await getTable(selectedProcessCode);
       setDataList(data); // 가져온 데이터를 상태에 저장
     };
-    fetchData(); // 함수 호출
+
+    fetchListData();
   }, [selectedProcessCode]);
 
   const handleDropdownChange = (processCode: string) => {
-    setSelectedProcessCode(processCode);
+    setSelectedProcessCode(processCode); // 공정 변경
   };
 
   return (
