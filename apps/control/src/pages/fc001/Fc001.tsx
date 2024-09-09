5c30ed2fb17822042839542a9cfc2012bcf840fd
dudco0040 <dudco0040@gmail.com>
2024-09-09 10:20:39 +0900

    Chore: merge 작업

--- apps/control/src/pages/fc001/Fc001.tsx
+++ apps/control/src/pages/fc001/Fc001.tsx
@@ -1,16 +1,13 @@
 import { Table, Tab } from '@postcoil/ui';
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 import styles from './Fc001.module.scss';
 import { RowheaderTable } from './rowheadertable/RowheaderTable';
 import { TopBar } from './topBar/TopBar';
 
-import {
-  columnsList,
-  columnsTable,
-  dataTable,
-} from '@/config/control/Fc001Utils';
+import { columnsList, columnsTable } from '@/config/control/Fc001Utils';
 
 // 작업대상재 추출 및 저장
 async function fetchTargetMaerialData() {
@@ -38,6 +35,19 @@ async function getTable(processCode: string) {
   }
 }
 
+// 작업대상재 표(table) 조회
+async function getRowTable() {
+  const url = `http://localhost:8086/control/fc001a/table`;
+  try {
+    const response = await axios.get(url);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
 export const Fc001: React.FC = () => {
   const label = ['리스트', '표']; // Tab
   const [isValue, setIsValue] = useState(true); // 기본값을 true로 설정(첫페이지)
@@ -46,6 +56,9 @@ export const Fc001: React.FC = () => {
   const [selectedProcessCode, setSelectedProcessCode] =
     useState<string>('1PCM'); // 선택된 공정
 
+  const [RowTableData, setRowTableData] = useState<any[]>([]);
+
+  // Tab 이동
   const changeTab = () => {
     setIsValue(!isValue);
   };
@@ -74,6 +87,17 @@ export const Fc001: React.FC = () => {
     setSelectedProcessCode(processCode); // 공정 변경
   };
 
+  useEffect(() => {
+    if (!isValue) {
+      const fetchData = async () => {
+        const data = await getRowTable(); // API 요청
+        setRowTableData(data);
+        console.log('표 불러오기 ');
+      };
+      fetchData();
+    }
+  }, [isValue]); // isValue가 변경될 때마다 실행
+
   return (
     <div className={styles.boardContainer}>
       <h1>공정별 작업대상재 관리</h1>
@@ -92,7 +116,13 @@ export const Fc001: React.FC = () => {
           </div>
         ) : (
           <div className={styles.customTable}>
-            <RowheaderTable columns={columnsTable} data={dataTable} />
+            <RowheaderTable
+              columns={columnsTable}
+              data={RowTableData.map((item, index) => ({
+                ...item,
+                key: index,
+              }))}
+            />
           </div>
         )}
       </div>
