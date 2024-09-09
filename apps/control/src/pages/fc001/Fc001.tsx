29090e3e5736c94802c54cdbf7831cf9e0afa63c
dudco0040 <dudco0040@gmail.com>
2024-09-09 15:30:03 +0900

    Chore: 코드 정리

--- apps/control/src/pages/fc001/Fc001.tsx
+++ apps/control/src/pages/fc001/Fc001.tsx
@@ -52,51 +52,48 @@ export const Fc001: React.FC = () => {
   const label = ['리스트', '표']; // Tab
   const [isValue, setIsValue] = useState(true); // 기본값을 true로 설정(첫페이지)
   const [dataList, setDataList] = useState<any[]>([]); // API로부터 받을 데이터 상태
-  // const [targetMaterialData, setTargetMaterialData] = useState<any[]>([]);
   const [selectedProcessCode, setSelectedProcessCode] =
     useState<string>('1PCM'); // 선택된 공정
   const [rowTableColumns, setRowTableColumns] = useState(
-    columnsTableConfig['default'],
+    columnsTableConfig['default'], // 1PCM
   );
   const [rowTableData, setRowTableData] = useState<any[]>([]);
 
-  // Tab 이동
+  // Select Tab
   const changeTab = () => {
     setIsValue(!isValue);
   };
 
-  // DropBox
+  // Select DropBox
   const handleDropdownChange = (processCode: string) => {
     console.log('Selected Process Code:', processCode);
     setSelectedProcessCode(processCode); // 공정 변경
-    console.log(columnsTableConfig[processCode]);
-    console.log(columnsTableConfig['default']);
 
     setRowTableColumns(
+      // 공정에 따라 컬럼 수정
       columnsTableConfig[processCode] || columnsTableConfig['default'],
     );
   };
 
-  // 컴포넌트가 마운트될 때 API로부터 데이터를 가져옴
+  // Effect: 컴포넌트가 마운트될 때 API로부터 데이터를 가져옴
+  // 1) 작업대상재 추출
   useEffect(() => {
     const fetchData = async () => {
-      // 1. 작업대상재 추출
       await fetchTargetMaerialData();
     };
-
     fetchData(); // 함수 호출
   }, []);
 
+  // 2) 작업대상재 목록 조회
   useEffect(() => {
     const fetchListData = async () => {
-      // 2. 1번 완료 후, 작업대상재 목록 조회
       const data = await getTable(selectedProcessCode);
       setDataList(data); // 가져온 데이터를 상태에 저장
     };
-
     fetchListData();
   }, [selectedProcessCode]);
 
+  // 3) 작업대상재 표 조회
   useEffect(() => {
     if (!isValue) {
       const fetchData = async () => {
