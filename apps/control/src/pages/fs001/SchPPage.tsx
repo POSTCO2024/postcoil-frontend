7af1a11350228f794e435b1f90b5ab5520f51ef2
sohyun-ahn <sohyun5747@gmail.com>
2024-09-20 10:32:36 +0900

    Feat: 경고 모달 추가

--- apps/control/src/pages/fs001/SchPPage.tsx
+++ apps/control/src/pages/fs001/SchPPage.tsx
@@ -5,7 +5,9 @@ import { useNavigate } from 'react-router-dom';
 import styles from './SchPPage.module.scss';
 
 import { fetchScheduleData } from '@/api/scheduleApi';
+import CommonModal from '@/components/common/CommonModal';
 import RollSuccessModal from '@/components/common/RollSuccessModal';
+import RollWarnModal from '@/components/common/RollWarnModal';
 import { MaterialDTO } from '@/config/scheduling/DTO';
 import ContentContainer from '@/pages/fs001/plan/ContentContainer';
 import FilterContainer from '@/pages/fs001/plan/FilterContainer';
@@ -13,6 +15,9 @@ import FilterContainer from '@/pages/fs001/plan/FilterContainer';
 const SchPPage = () => {
   const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModal2Open, setIsModal2Open] = useState(false);
+  const [isModal3Open, setIsModal3Open] = useState(false);
+
   const handleCancel = () => {
     setIsModalOpen(false);
     navigate('/schedule2');
@@ -21,7 +26,28 @@ const SchPPage = () => {
     setIsModalOpen(false);
     navigate('/schedule2');
   };
-  const handleRoll = () => setIsModalOpen(true);
+
+  const handleApply2 = () => {
+    setIsModalOpen(true);
+    setIsModal2Open(false);
+  };
+
+  const handleCancel2 = () => {
+    setIsModal2Open(false);
+  };
+
+  const handleCancel3 = () => {
+    setIsModal3Open(false);
+  };
+
+  const handleRoll = () => {
+    if (selectedRows.length < 1) {
+      setIsModal3Open(true);
+      return;
+    }
+
+    setIsModal2Open(true);
+  };
 
   const [materialData, setMaterialData] = useState<MaterialDTO[]>([]);
 
@@ -52,9 +78,33 @@ const SchPPage = () => {
       <h1>Schedule 편성</h1>
       <FilterContainer handleChange={handleChange} />
       <ContentContainer data={materialData} setSelectedRows={setSelectedRows} />
-      <Button type="primary" className={styles.btn} onClick={handleRoll}>
+      <Button
+        style={{ display: materialData.length > 0 ? 'block' : 'none' }}
+        type="primary"
+        className={styles.btn}
+        onClick={handleRoll}>
         스케줄 편성 시작
       </Button>
+      <RollWarnModal
+        isModalOpen={isModal3Open}
+        handleCancel={handleCancel3}
+        handleApply={handleCancel3}
+        title={'재료를 1개 이상 선택해주세요.'}
+      />
+      <CommonModal
+        isModalOpen={isModal2Open}
+        isConfirmation={false}
+        onCancel={handleCancel2}
+        onApply={handleApply2}>
+        <p
+          style={{
+            fontSize: '1.5rem',
+            textAlign: 'center',
+            marginTop: '30px',
+          }}>
+          {`${selectedRows.length}개의 재료로 스케줄을 편성하시겠습니까?`}
+        </p>
+      </CommonModal>
       <RollSuccessModal
         isModalOpen={isModalOpen}
         handleCancel={handleCancel}
