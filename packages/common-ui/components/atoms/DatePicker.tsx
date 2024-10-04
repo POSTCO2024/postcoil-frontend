a62db2bdeec2e4a44932e81e3017830b2bdc1f25
sohyun-ahn <sohyun5747@gmail.com>
2024-10-04 17:39:31 +0900

    Feat: 세팅 value 추가

--- packages/common-ui/components/atoms/DatePicker.tsx
+++ packages/common-ui/components/atoms/DatePicker.tsx
@@ -1,15 +1,33 @@
 import { DatePicker as AntdDatePicker } from 'antd';
 import dayjs, { Dayjs } from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 dayjs.extend(customParseFormat);
 import styles from './DatePicker.module.scss';
 
 export const DatePicker: React.FC<{
+  dateRangeString?: string | null;
   onChange?: (dateString?: string) => void;
-}> = ({ onChange }) => {
+}> = ({ dateRangeString, onChange }) => {
   const { RangePicker } = AntdDatePicker;
+
+  const convertDates = (
+    dateRangeString: string | null,
+  ): [dayjs.Dayjs | null, dayjs.Dayjs | null] => {
+    if (dateRangeString && dateRangeString !== '') {
+      const [startStr, endStr] = dateRangeString.split(' - ');
+      const startDate = dayjs(startStr); // 문자열을 Dayjs 객체로 변환
+      const endDate = dayjs(endStr); // 문자열을 Dayjs 객체로 변환
+      return [startDate, endDate];
+    } else {
+      return [null, null];
+    }
+  };
+
+  const [dateRangeStr, setDateRangeStr] = useState<
+    [dayjs.Dayjs | null, dayjs.Dayjs | null]
+  >(convertDates(dateRangeString!));
 
   const disabledDate = (current: dayjs.Dayjs) => {
     // Can not select days before today and today
@@ -22,11 +40,17 @@ export const DatePicker: React.FC<{
       const endDate = dates[1].toDate(); // Dayjs 객체를 Date 객체로 변환
 
       const formattedRange = `${startDate.toLocaleDateString()} - ${endDate.toLocaleDateString()}`;
+      setDateRangeStr(dates);
       onChange?.(formattedRange);
     } else {
-      onChange?.();
+      setDateRangeStr([null, null]);
+      onChange?.('');
     }
   };
+
+  useEffect(() => {
+    setDateRangeStr(convertDates(dateRangeString!));
+  }, [dateRangeString]);
 
   return (
     <div className={styles.datepickerContainer}>
@@ -36,6 +60,7 @@ export const DatePicker: React.FC<{
         status="error"
         disabledDate={disabledDate}
         onChange={(dates) => handleRangeChange(dates as [Dayjs, Dayjs])}
+        value={dateRangeStr}
         className={styles.datepicker}
       />
       <span className={styles.spacer}></span>
