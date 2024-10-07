923dd2207db45b061a6d7df47e80be1a430252f6
sohyun-ahn <sohyun5747@gmail.com>
2024-10-07 23:31:00 +0900

    Fix: main.tsx StrictMode 재활성화

--- apps/control/src/main.tsx
+++ apps/control/src/main.tsx
@@ -1,7 +1,8 @@
-// import { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+
 import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
@@ -9,4 +10,3 @@ createRoot(document.getElementById('root')!).render(
     <App />,
   </StrictMode>,
 );
-
